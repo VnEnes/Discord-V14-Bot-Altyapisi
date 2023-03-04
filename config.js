b9ebc1a88de08ace25b0e20a90871b648d8efe12
VnEnes <123584405+VnEnes@users.noreply.github.com>
2023-03-04 14:26:48 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  "prefix": ".",
+  "token": "MTAxMDA4NTc5MzE0ODE4MjU5OA.GlbeFC.w9Msi-LfwAqtfYDKLXbGu881q8Rxr5QTIi1CuY"
+}
---
+++ index.js
@@ -0,0 +1,25 @@
+const { Client, GatewayIntentBits, Partials, ButtonBuilder, ButtonStyle, ActionRowBuilder, ModalBuilder, TextInputBuilder, TextInputStyle, InteractionType } = require("discord.js");
+const Discord = require("discord.js")
+const config = require("./config.js");
+let prefix = config.prefix
+const db = require("quick.db")
+const INTENTS = Object.values(GatewayIntentBits);
+const PARTIALS = Object.values(Partials);
+const client = global.client = new Discord.Client({
+    intents: INTENTS,
+    allowedMentions: {
+        parse: ["users"]
+    },
+    partials: PARTIALS,
+    retryLimit: 3
+});
+
+
+module.exports = client;
+
+require("./events/message.js") 
+
+require("./events/ready.js")
+
+
+client.login(config.token)
